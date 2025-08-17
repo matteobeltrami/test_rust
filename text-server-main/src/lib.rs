1ad7651e5775edbe36ab987fb16d27891b65b07e
Matteo Beltrami <beltramimatteo01@gmail.com>
2025-08-17 11:56:29 +0200

    remove text-server

--- text-server-main/src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub mod server;
-pub mod errors;
-pub mod assembler;
-pub mod text_server;
-
-// Re-export main types for easier use
-pub use server::Server;
-pub use text_server::TextServer;
-pub use assembler::Assembler;
